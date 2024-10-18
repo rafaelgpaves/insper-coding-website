ba9f47b4dc967995804e385c714fb6acdfc5d4ec
Henrique Mayor <henriquessm@al.insper.edu.br>
2024-10-18 01:09:05 -0300

    parceiros e cases de sucesso

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
+import CarouselComponent from './CarouselComponent';
 
 import cloud_icon from './assets/imgs/cloud.png';
 import data_icon from './assets/imgs/dados.png';
@@ -11,19 +12,22 @@ import games_icon from './assets/imgs/games.png';
 import web_icon from './assets/imgs/web.png';
 import ia_icon from './assets/imgs/ia.png';
 
-import Paper from '@mui/material/Paper';
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#1A2027',
-  }),
-}));
+
+import insper from './assets/imgs/logos/insper.png';
+import inhealth from './assets/imgs/logos/inhealth.png';
+import fundacao from './assets/imgs/logos/fundacao.png';
+import insperdata from './assets/imgs/logos/insperdata.png';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const logosSlide = document.querySelector(".logos-slide");
+    if (logosSlide) {
+      const copy = logosSlide.cloneNode(true);
+      document.querySelector(".logos").appendChild(copy);
+    }
+  }, []); // O array vazio significa que o efeito será executado apenas uma vez, após a montagem
+
   return (
     <>
       <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
@@ -33,7 +37,7 @@ function App() {
 
       <div className='cultura_titulo'> 
           <h2>Nossa Cultura</h2>
-          <body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque molestie leo, non pretium massa. Phasellus nibh magna, ultricies in leo sed, tempor malesuada libero. Nulla vehicula, mauris id scelerisque placerat, odio mi pellentesque purus, eu dignissim tellus odio non lorem.</body>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque molestie leo, non pretium massa. Phasellus nibh magna, ultricies in leo sed, tempor malesuada libero. Nulla vehicula, mauris id scelerisque placerat, odio mi pellentesque purus, eu dignissim tellus odio non lorem.</p>
       </div>
 
       <div>
@@ -41,7 +45,7 @@ function App() {
           <Grid container columnSpacing={10}>
             <Grid size={4}>
               <h3 className = 'valores'>Valores</h3>
-              <hr class='line'></hr>
+              <hr className='line'></hr>
               <body className='valores_texto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque molestie leo, non pretium massa. Phasellus nibh magna, ultricies in leo sed, tempor malesuada libero.Donec ut suscipit turpis. Aenean elit tortor, scelerisque eu pretium at, facilisis ut lorem.</body>
               
             </Grid>
@@ -124,10 +128,33 @@ function App() {
         </Box>
       </div>
       <hr className='division'></hr>
+      
+      <div className='parceiros_titulo'> 
+          <h2>Nossos Parceiros</h2>
+      </div>  
 
+      <hr className='division_slide_up'></hr>
+      <div className='parceiros_texto'>
+        <div className="logos">
+          <div className="logos-slide">
+            <img src={insper} alt="Insper Logo" />
+            <img src={inhealth} alt="InHealth Logo" />
+            <img src={fundacao} alt ="Fundação Logo" />
+            <img src={insperdata} alt ="InsperData Logo" />
+          </div>
+
+        </div>
+      </div>
+      <hr className='division_slide_down'></hr>
+      <hr className='division'></hr>
+      <div className='parceiros_titulo'> 
+          <h2>Cases de Sucesso</h2>
+      </div>  
+      <CarouselComponent />
 
     </>
   );
 }
 
 export default App;
+
